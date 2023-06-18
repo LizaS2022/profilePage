63fb3864edc2ab04b12fab70fb54daa99dc7c004
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-06-18 10:59:25 -0400

    wrote p

--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,50 @@
-import React, { Component } from 'react';
-import {Container,Row,Col} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const styles = {
-  profileImg:{
-    height:"120px",
+  profileImg: {
+    height: "120px",
     width: "120px",
     borderRadius: "50%",
-  }
-}
+  },
+};
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <Container id='about' className='mt-5 pt-5'>
-           
-            <h1>About Me</h1>
-            <Row className='mb-3'>
-                <Col>
-                    <img style={styles.profileImg} src={process.env.PUBLIC_URL + './profile.jpg'}  alt="Profile" />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p>
-                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
-                    </p>
-               </Col>
-            </Row>
-            
+        <Container id="about" className="mt-5 pt-5">
+          <h1>About Me</h1>
+          <Row className="mb-3">
+            <Col>
+              <img
+                style={styles.profileImg}
+                src={process.env.PUBLIC_URL + "./profile.jpg"}
+                alt="Profile"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>
+                Having a strong foundation in education, I am passionately
+                transitioning into the tech industry, eager to apply my
+                problem-solving skills in a new context. I have recently
+                completed an intensive Full Stack Development bootcamp at
+                Georgia Tech, where I honed my abilities in modern technologies
+                such as JavaScript, React, Node.js, and more. My background in
+                education has instilled in me the importance of continuous
+                learning, collaboration, and effective communication, all of
+                which are crucial in the world of software development. Now, I
+                am eager to start my journey as an entry-level full stack
+                developer, keen on bringing fresh perspectives to the table and
+                eager to contribute my learned skills to a dynamic and
+                innovative team.
+              </p>
+            </Col>
+          </Row>
         </Container>
-     </div>
-    )
+      </div>
+    );
   }
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react'
-import {Form, Button, Container} from 'react-bootstrap'
+import React, { Component } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
 export default class Contact extends Component {
   render() {
     return (
       <div>
-        <Container id='contact' className='mt-5 pt-5'>
-        <Form>
+        <Container id="contact" className="mt-5 pt-5">
+          <Form>
             <div>
-            <h1>Message Me</h1>
+              <h1>Message Me</h1>
             </div>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="name" placeholder="John Smith" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Send
-      </Button>
-    </Form>
-    </Container>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="name" placeholder="John Smith" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Message Me</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Send
+            </Button>
+          </Form>
+        </Container>
       </div>
-    )
+    );
   }
 }
