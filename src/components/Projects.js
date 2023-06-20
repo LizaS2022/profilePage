ffa1ace7f1a82d0d0040fc2941b5c781cab5a615
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-06-19 23:26:02 -0400

    added project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,32 @@
+const Projects = [
+  {
+    id: 1,
+    projectImg: "./dev-blog.gif",
+    title: "Dev-Blog",
+    DeployedURL: "https://dev-blog11.herokuapp.com/login",
+    GitHubLink: "https://github.com/LizaS2022/dev-blog.git",
+  },
+  {
+    id: 2,
+    projectImg: "./noteTaker.gif",
+    title: "Note-Taker",
+    DeployedURL: "https://note-taker-33.herokuapp.com/",
+    GitHubLink: "https://github.com/LizaS2022/noteTaker.git",
+  },
+  {
+    id: 3,
+    projectImg: "./ecommerce-app.gif",
+    title: "Ecommerce-App",
+    DeployedURL: "",
+    GitHubLink: "https://github.com/LizaS2022/ecommerce-app-project.git",
+  },
+  {
+    id: 4,
+    projectImg: "./ecommerce-app.gif",
+    title: "UrgentReserve",
+    DeployedURL: "https://rapid-checkin-98d3ad3f1287.herokuapp.com/",
+    GitHubLink: "https://github.com/ParisaDarkhal/Urgent-Care-Checkin.git",
+  },
+];
 
-        const Projects = [
-            {
-                id:1,
-                projectImg: "./dev-blog.gif",
-                title: "Dev-Blog",
-                DeployedURL: "https://dev-blog11.herokuapp.com/login",
-                GitHubLink: "https://github.com/LizaS2022/dev-blog.git",
-            },
-              {
-                id:2,
-                projectImg: "./noteTaker.gif",
-                title: "Note-Taker",
-                DeployedURL: "https://note-taker-33.herokuapp.com/",
-                GitHubLink: "https://github.com/LizaS2022/noteTaker.git",
-            },
-              {
-                id:3,
-                projectImg: "./ecommerce-app.gif",
-                title: "Ecommerce-App",
-                DeployedURL: "",
-                GitHubLink: "https://github.com/LizaS2022/ecommerce-app-project.git",
-            },
-        ]
-
-        export default Projects;
+export default Projects;
